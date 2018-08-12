6e794942928336d92c8837a293928b647b240834
Chad <sprongc@nevada.unr.edu>
2018-08-12 12:42:55 -0700

    buttons added

--- javascript/script.js
+++ javascript/script.js
@@ -24,5 +24,50 @@ function lastLetter(input)
     return result;
 }
 
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+const p = document.createElement('p');
+p.classList.add('p');
+p.textContent = "Hey, I am red!";
+p.style.color = "red";
+
+const h3 = document.createElement('h3');
+h3.classList.add('h3');
+h3.textContent = "Hey, I am blue h3!";
+h3.style.color = "blue";
+
+content.appendChild(h3);
+
+container.appendChild(content);
+container.appendChild(p);
+//container.appendChild(h3);
+
+var button = document.querySelector('#btn');
+button.onclick = () => alert('Hello');
+
+var btn = document.querySelector('#btn1');
+btn.addEventListener( 'click', alerting );
+btn.addEventListener( 'click', function(e)
+    {
+        console.log(e);
+        e.target.style.background = 'blue';
+    });
 
 
+function alerting()
+{
+    alert("this is the perferred method")
+}
+
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => 
+    {
+        alert(button.id);
+    });
+});
