3a43f2b5c7f1f9f8cdb24f7b43267c59c2b62c2a
Chad <sprongc@nevada.unr.edu>
2018-09-08 16:05:39 -0700

    added calculator

---
+++ blogging-app/README.md
@@ -0,0 +1,5 @@
+This is the blogger2 application found in the tutorial from http://tutorials.jumpstartlab.com/projects/blogger.html
+
+The user can upload an article, use tags, and upload a picture.
+
+The systems relies on imagemagick for picture resizing
--- calculator/script.js
+++ calculator/script.js
@@ -1,41 +1,96 @@
 
-function operate( operation, one, two )
-{
-  switch(operation){
-    case 0:
-      return add(one, two);
-    case 1:
-      return subtract(one, two);
-    case 2:
-      return multiply(one, two);
-    case 3:
-      return divide(one, two);
-    case 4:
-      return modulo(one, two);
+var operandX = "";
+var operandY = "";
+var operator = "";
+let operator_pressed = false;
+//event handlers
+
+ 
+
+document.getElementById("numbers").addEventListener("click", function(event) {
+  var num = event.target.innerText;
+  if( operator_pressed )
+  {
+    operandY += num;
+    setDisplayText(operandY);
   }
+  else
+  {
+    operandX += num;
+    setDisplayText(operandX);
+  }
+  console.log(operandX);
+})
+
+document.getElementById("operators").addEventListener("click", function(event) {
+  operator_pressed = true;
+  operator = event.target.innerText;
+  console.log(event.target.dataset.operator);
+})
+
+document.getElementById("equals").addEventListener("click", function(event) {
+  operator_pressed = false;
+  calc( operator, operandX, operandY );
+})
+
+document.getElementById("clear").addEventListener("click", function(event) {
+  clear();
+})
+
+function clear()
+{
+  operator_pressed = false;
+  operandX = "";
+  operandY = "";
+  setDisplayText(0);
+}
+
+function calc ( operator, x, y )
+{
+  operator_pressed = false;
+  switch(operator){
+    case '+':
+      setDisplayText( add( x, y) );
+      break;
+    case "/":
+      setDisplayText( divide( x, y) );
+      break;
+    case '*':
+      setDisplayText( multiply( x, y) );
+      break;
+    case '-':
+      setDisplayText( subtract( x, y) );
+       break;
+  }
+  operandY = "";
+}
+
+function setDisplayText(text) {
+  var display = document.querySelector(`#display`);
+  display.textContent = text;
 }
 
 function add(one, two)
 {
-  return one + two;
+  operandX = parseFloat(one) + parseFloat(two)
+  return operandX;
 }
 
 function subtract(one, two)
 {
-  return one - two;
+  operandX = parseFloat(one) - parseFloat(two)
+  return operandX;
 }
 
 function multiply(one, two)
 {
-  return one * two;
+  operandX = parseFloat(one) * parseFloat(two)
+  return operandX;
 }
 
 function divide(one, two)
 {
-  return one / two;
+  operandX = parseFloat(one) / parseFloat(two)
+  return operandX;
 }
 
-function modulo(one, two)
-{
-  return one % two;
-}
