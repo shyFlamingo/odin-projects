830bbff636a2b096494949edd6a4b6ed0a6c9808
Chad <sprongc@nevada.unr.edu>
2018-09-08 10:23:09 -0700

    completed tagging functions

--- blogging-app/blogger/app/models/article.rb
+++ blogging-app/blogger/app/models/article.rb
@@ -1,3 +1,15 @@
 class Article < ApplicationRecord
-  has_many :comments
+  has_many :comments,  dependent: :destroy
+  has_many :taggings,  dependent: :destroy
+  has_many :tags, through: :taggings
+
+  def tag_list
+		tags.join(", ")
+	end
+
+	def tag_list=(tags_string)
+		tag_list = tags_string.split(',').map { |tag| tag.strip.downcase }.uniq
+		self.tags = tag_list.map { |tag| Tag.find_or_create_by(name: tag) }
+end
+
 end
